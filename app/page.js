71997cfbc62e388fdcb528b149e6d1676009f37f
Justin Byrd <justinbyrd7@gmail.com>
2024-02-17 16:53:16 -0600

    wip

--- app/page.js
+++ app/page.js
@@ -3,21 +3,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 border border-red-500 border-solid">
-      <div className=" max-w-7xl w-full items-left justify-between text-sm lg:flex flex-col border border-blue-500 border-solid">
-        <h1 className="border border-solid border-white">
+      <div className=" max-w-7xl w-full items-left justify-between text-sm lg:flex flex-col border-2 border-blue-500 border-solid">
+        <h1 className="border border-solid border-black">
           Header -left: logo -right: text: Home, button: Get In Touch (route
           this to /contact)
         </h1>
 
-        <div className="border border-dashed border-white">
+        <div className="border border-dashed border-black">
           Body: 'justin_byrd' and socials below that
         </div>
-        <h2 className="border border-dashed border-white ">About Me</h2>
-        <h2 className="border border-dashed border-white">
+        <h2 className="border border-dashed border-black ">About Me</h2>
+        <h2 className="border border-dashed border-black">
           Project 1(maybe only one until CMS gets installed)
         </h2>
-        <h2 className="border border-dashed border-white">Project 2</h2>
-        <h2 className="border border-dashed border-white">Project 3</h2>
+        <h2 className="border border-dashed border-black">Project 2</h2>
+        <h2 className="border border-dashed border-black">Project 3</h2>
       </div>
     </main>
   );

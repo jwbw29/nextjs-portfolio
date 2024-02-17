5ff055477b355450b8e8aea2d016c1ff16f7e1f8
Justin Byrd <justinbyrd7@gmail.com>
2024-02-16 18:33:44 -0600

    wip

---
+++ app/contact/page.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <div className="max-w-5xl w-full items-left justify-between font-mono text-sm lg:flex flex-col">
+        <div>
+          Header -left: logo -right: text: Home, button: Get In Touch (route
+          this to /contact)
+        </div>
+        <div>Body: 'justin_byrd' and socials below that</div>
+        <div>About Me</div>
+        <div>Project 1(maybe only one until CMS gets installed)</div>
+        <div>Project 2</div>
+        <div>Project 3</div>
+      </div>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 border border-red-500 border-solid">
-      <div className="max-w-5xl w-full items-left justify-between font-mono text-sm lg:flex flex-col border border-blue-500 border-solid">
+      <div className=" max-w-7xl w-full items-left justify-between font-mono text-sm lg:flex flex-col border border-blue-500 border-solid">
         <div className="border border-solid border-white">
           Header -left: logo -right: text: Home, button: Get In Touch (route
           this to /contact)

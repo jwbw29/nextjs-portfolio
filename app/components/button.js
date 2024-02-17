002cd5c58e8b431f827e0b84fcdd6293d734190f
Justin Byrd <justinbyrd7@gmail.com>
2024-02-17 16:45:39 -0600

    styling theme

---
+++ app/components/button.js
@@ -0,0 +1,8 @@
+//primary button
+export const PrimaryButton = () => {};
+
+//secondary button
+export const SecondaryButton = () => {};
+
+//Contact ME button
+export const ContactButton = () => {};
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
-      <div className="max-w-5xl w-full items-left justify-between font-mono text-sm lg:flex flex-col">
+      <div className="max-w-5xl w-full items-left justify-between text-sm lg:flex flex-col">
         <div>
           Header -left: logo -right: text: Home, button: Get In Touch (route
           this to /contact)

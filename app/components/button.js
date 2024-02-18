dfb48c87375f01853b92a1a43a032881f65b6356
Justin Byrd <justinbyrd7@gmail.com>
2024-02-17 20:45:49 -0600

    button components created and styled

--- app/components/button.js
+++ app/components/button.js
@@ -1,8 +1,26 @@
 //primary button
-export const PrimaryButton = () => {};
+export const PrimaryButton = ({ children }) => {
+  return (
+    <button className=" h-14 w-32 text-2xl text-dark bg-white rounded-full focus:shadow-outline  hover:shadow-md hover:shadow-white">
+      {children}
+    </button>
+  );
+};
 
 //secondary button
-export const SecondaryButton = () => {};
+export const SecondaryButton = ({ children }) => {
+  return (
+    <button className=" h-14 w-32 text-2xl text-white bg-none border border-solid border-white rounded-full focus:shadow-outline  hover:shadow-md hover:shadow-white">
+      {children}
+    </button>
+  );
+};
 
 //Contact ME button
-export const ContactButton = () => {};
+export const ContactButton = ({ children }) => {
+  return (
+    <button className=" h-14 w-44 text-2xl text-white bg-none border border-solid border-white rounded-full focus:shadow-outline  hover:shadow-md hover:shadow-white">
+      {children}
+    </button>
+  );
+};

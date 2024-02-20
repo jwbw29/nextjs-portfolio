7c7481dbc6faee2a36af1d016a1c0eb6cd2c7d3c
Justin Byrd <justinbyrd7@gmail.com>
2024-02-20 12:29:08 -0600

    Update header component and favicon

--- app/components/header.js
+++ app/components/header.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import { ContactButton } from "./Button"; // Adjust the import path as needed
+import { ContactButton } from "./Button";
+
+import logo from "../../public/JB Logo.png";
 
 const Header = () => {
   return (
     <header className="flex h-48 justify-between items-center py-6">
       <div className="flex items-center">
-        {/* <Image src="/logo.svg" alt="Logo" width={40} height={40} /> */}
-        <h1>image</h1>
+        <Image src={logo} alt="Logo" width={160} height={160} />
       </div>
       <div className="flex items-center gap-6">
         <h3 className="text-2xl ml-4">Home</h3>

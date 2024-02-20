855c081dc34de07a6bc9f7cb3aa87ade4ba9f7fd
Justin Byrd <justinbyrd7@gmail.com>
2024-02-20 12:19:07 -0600

    Update component styles

--- app/page.js
+++ app/page.js
@@ -8,11 +8,11 @@ import projectsData from "../public/projectsData.json";
 
 export default function Home() {
   return (
-    <main className="bg-dark text-white flex min-h-screen flex-col items-center p-6 border border-red-500 border-solid">
+    <main className="bg-dark text-white flex min-h-screen flex-col items-center p-6 pt-0 border border-red-500 border-solid">
       <div className=" max-w-7xl w-full items-left justify-between text-sm lg:flex flex-col border-2 border-blue-500 border-solid">
-        <div className="h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col border border-dashed border-green-700">
           <Header />
-          <div className="flex flex-col gap-10 border border-solid border-white items-center justify-center flex-grow">
+          <div className="flex flex-col gap-10 border border-solid border-white items-center justify-center flex-1">
             <h1>justin_byrd</h1>
             <h3>Socials</h3>
           </div>

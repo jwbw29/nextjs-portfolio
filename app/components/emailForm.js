8c47ee3c9958bf38e6f7bfdb85a92e27fcbfe4e0
Justin Byrd <justinbyrd7@gmail.com>
2024-02-22 11:09:16 -0600

    updating

---
+++ app/components/emailForm.js
@@ -0,0 +1,49 @@
+import { PrimaryButton } from "./button";
+
+const EmailForm = () => {
+  return (
+    // * Use this link to build the functionality: https://www.youtube.com/watch?v=t2LvPXHLrek
+
+    <div className="size-full ring-1 ring-white rounded-lg p-8">
+      <form>
+        <div className="flex flex-wrap mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label>First Name (required)</label>
+            <input type="text" />
+          </div>
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label>Last Name (required)</label>
+            <input type="text" />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap mb-6">
+          <div className="w-full px-3">
+            <label>Email (required)</label>
+            <input type="email" />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap mb-6">
+          <div className="w-full px-3">
+            <label>Company</label>
+            <input type="text" />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap mb-6">
+          <div className="w-full px-3">
+            <label>Message (required)</label>
+            <textarea rows="5" />
+          </div>
+        </div>
+
+        <div className="flex px-3 justify-end">
+          <PrimaryButton type="submit">Send Message</PrimaryButton>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EmailForm;
---
+++ app/page.js
@@ -0,0 +1,25 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Hero from "./components/hero";
+import AboutMe from "./components/about";
+import Project from "./components/project";
+
+import projectsData from "../public/projectsData.json";
+
+export default function Home() {
+  return (
+    <main className="bg-dark text-white flex min-h-screen flex-col items-center p-6 pt-0">
+      <div className=" max-w-7xl w-full items-left justify-between text-sm lg:flex flex-col ">
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <Hero />
+        </div>
+        <AboutMe />
+        {projectsData.map((project, i) => (
+          <Project key={i} project={project} />
+        ))}
+      </div>
+      <Footer />
+    </main>
+  );
+}

405b110fb85d4c39484a838db3cbf34ac369900a
Justin Byrd <justinbyrd7@gmail.com>
2024-02-22 12:29:42 -0600

    updated unescaped entities

--- app/contact/page.js
+++ app/contact/page.js
@@ -11,36 +11,36 @@ const Contact = () => {
         <div className="flex max-w-full py-12 px-6 ">
           <div className="flex flex-col w-1/2 m-4 gap-16 ">
             <div>
-              <h1>Let's Connect!</h1>
+              <h1>{"Let's Connect!"}</h1>
             </div>
 
             <div className="flex flex-col gap-8 ">
               <div className="flex flex-col  gap-1 ">
                 <div className="flex flex-1 items-center">
-                  <h3 className="text-2xl">Location </h3>
+                  <h3 className="text-2xl">{"Location"} </h3>
                 </div>
                 <div className="flex flex-1 items-center text-gray-400">
-                  <p className="text-lg">📍 Missouri, United States </p>
+                  <p className="text-lg">{"📍 Missouri, United States "}</p>
                 </div>
               </div>
 
               <div className="flex flex-col  gap-1 ">
                 <div className="flex flex-1 items-center">
-                  <h3 className="text-2xl">Email </h3>
+                  <h3 className="text-2xl">{"Email"} </h3>
                 </div>
                 <div className="flex flex-1 items-center text-gray-400">
                   <a
                     href="mailto:justinbyrd7@gmail.com"
                     className=" hover:text-blue-300"
                   >
-                    <p className="text-lg">✉️ justinbyrd7@gmail.com </p>
+                    <p className="text-lg">{"✉️ justinbyrd7@gmail.com "}</p>
                   </a>
                 </div>
               </div>
 
               <div className="flex flex-col  gap-1 ">
                 <div className="flex flex-1 items-center">
-                  <h3 className="text-2xl">Schedule </h3>
+                  <h3 className="text-2xl">{"Schedule"} </h3>
                 </div>
                 <div className="flex flex-1 items-center text-gray-400">
                   <a
@@ -48,7 +48,7 @@ const Contact = () => {
                     target="_blank"
                     className=" hover:text-blue-300"
                   >
-                    <p className="text-lg">📆 calendly.com/justin-byrd7</p>
+                    <p className="text-lg">{"📆 calendly.com/justin-byrd7"}</p>
                   </a>
                 </div>
               </div>
--- components/about.js
+++ components/about.js
@@ -8,15 +8,12 @@ const AboutMe = () => {
       {" "}
       <div className="flex flex-col w-1/2 gap-12 justify-center">
         <div className="flex flex-col items-start">
-          <h2>About Me</h2>
+          <h2>{"About Me"}</h2>
         </div>
         <p className="text-xl text-left pr-8">
-          I'm a former Product Owner who recently became a Full-Stack Engineer.
-          I have years of experience in Product Strategy and Development,
-          learning from users and how to build a user-friendly product. I wanted
-          to do more of the building myself and decided to become a full-stack
-          engineer. I enjoy learning new and better ways to do things and then
-          applying those skills to see an idea come to reality. 
+          {
+            "I'm a former Product Owner who recently became a Full-Stack Engineer. I have years of experience in Product Strategy and Development, learning from users and how to build a user-friendly product. I wanted to do more of the building myself and decided to become a full-stack engineer. I enjoy learning new and better ways to do things and then applying those skills to see an idea come to reality."
+          }
         </p>
       </div>
       <div className="flex flex-col gap-6 w-1/2 items-center justify-center">

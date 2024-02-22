40c2e019b7fafb7866840d3d2e1f56f4df0bf43f
Justin Byrd <justinbyrd7@gmail.com>
2024-02-22 10:37:59 -0600

    Update page.js styling

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import projectsData from "../public/projectsData.json";
 export default function Home() {
   return (
     <main className="bg-dark text-white flex min-h-screen flex-col items-center p-6 pt-0">
-      <div className=" max-w-7xl w-full items-left justify-between text-sm lg:flex flex-col">
+      <div className=" max-w-7xl w-full items-left justify-between text-sm lg:flex flex-col ">
         <div className="min-h-screen flex flex-col">
           <Header />
           <Hero />

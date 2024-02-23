bdc55cfdf0693af4defe52d4cd30754f40263742
Justin Byrd <justinbyrd7@gmail.com>
2024-02-23 17:27:54 -0600

    Add responsive design to AboutMe and Project components

--- components/about.js
+++ components/about.js
@@ -5,23 +5,23 @@ import portrait from "../public/portrait.jpg";
 const AboutMe = () => {
   return (
     //TODO
-    <div className="flex min-h-screen gap-4 text-4xl  py-24 ">
+    <div className="flex flex-col lg:flex-row min-h-screen gap-16 lg:gap-4 text-4xl py-24 ">
       {" "}
       {/* TODO */}
-      <div className="flex flex-col w-1/2 gap-12 justify-center">
+      <div className="flex flex-col lg:w-1/2 gap-12 justify-center">
         {/* TODO */}
-        <div className="flex flex-col items-start">
+        <div className="flex flex-col items-center lg:items-start">
           {/* TODO */} <h2>{"About Me"}</h2>
         </div>
         {/* TODO */}
-        <p className="text-xl text-left pr-8">
+        <p className="text-xl text-justify lg:text-left lg:pr-8">
           {
             "I'm a former Product Owner who recently became a Full-Stack Engineer. I have years of experience in Product Strategy and Development, learning from users and how to build a user-friendly product. I wanted to do more of the building myself and decided to become a full-stack engineer. I enjoy learning new and better ways to do things and then applying those skills to see an idea come to reality."
           }
         </p>
       </div>
       {/* TODO */}
-      <div className="flex flex-col gap-6 w-1/2 items-center justify-center">
+      <div className="flex flex-col gap-6 lg:w-1/2 items-center justify-center">
         {/* TODO */}
         <Image
           className=" rounded-3xl"
@@ -30,6 +30,9 @@ const AboutMe = () => {
           width={400}
           alt="Picture of the project"
         />
+      </div>
+      <div className="lg:hidden flex justify-center mt-10">
+        <div className="border-b-2 border-white w-2/3"></div>
       </div>
     </div>
   );
--- components/project.js
+++ components/project.js
@@ -4,16 +4,18 @@ import { PrimaryButton, SecondaryButton } from "./button";
 
 const Project = ({ project }) => {
   return (
-    <div className="flex min-h-screen text-4xl gap-12 py-24 ">
+    <div className="flex flex-col lg:flex-row min-h-screen text-4xl gap-12 py-24 ">
       {" "}
-      <div className="flex flex-col w-1/2 gap-12 justify-center">
-        <div className="flex flex-col items-start">
+      <div className="flex flex-col lg:w-1/2 gap-16 lg:gap-12 justify-center">
+        <div className="flex flex-col items-center lg:items-start">
           <h2>{project.name}</h2>
           <h4>{project.subtitle}</h4>
         </div>
-        <p className="text-xl text-left pr-8">{project.description}</p>
+        <p className="text-xl text-justify lg:text-left lg:pr-8">
+          {project.description}
+        </p>
       </div>
-      <div className="flex flex-col gap-6 w-1/2 items-center justify-center">
+      <div className="flex flex-col gap-6 lg:w-1/2 items-center justify-center">
         <Image
           className=" rounded-3xl"
           src={project.image}
@@ -29,6 +31,9 @@ const Project = ({ project }) => {
             <SecondaryButton>CODE</SecondaryButton>
           </Link>
         </div>
+      </div>
+      <div className="lg:hidden flex justify-center mt-8">
+        <div className="border-t-2 border-white w-2/3"></div>
       </div>
     </div>
   );
